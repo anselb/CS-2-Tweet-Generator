b3b16092b75de1d9052c5cd5a463bebc8c1f56b8
Ansel Bridgewater <iamansel@gmail.com>
2017-10-26 17:34:45 -0700

    Finished first two required challenges, started anagram

---
+++ dictionary_words.py
@@ -0,0 +1,15 @@
+import sys
+from random import randint
+
+
+if __name__ == "__main__":
+    input_sentence_len = int(sys.argv[1])
+    chosen_words = []
+
+    with open('/usr/share/dict/words', 'r') as f:
+        dict_words = f.readlines()
+        for _ in range(input_sentence_len):
+            rand_num = randint(0, len(dict_words) - 1)
+            chosen_words.append(dict_words[rand_num].replace('\n', ''))
+
+    print(' '.join(chosen_words))
---
+++ rearrange.py
@@ -0,0 +1,22 @@
+import sys
+import random
+
+
+def rearange_words(input_words):
+    '''
+    Takes a list of words and prints the words in a random order
+    input_words is a list of words
+    '''
+    new_word_order = []
+    for words in range(len(input_words)):
+        rand_word_index = random.randint(0, len(input_words) - 1)
+        new_word_order.append(input_words[rand_word_index])
+        print(input_words[rand_word_index], end=" ")
+        del input_words[rand_word_index]
+    # print('')
+    # print(' '.join(new_word_order))
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    rearange_words(arguments)
