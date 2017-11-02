df8797103b8d9a628c9037745f85e32c16441b8e
Ansel Bridgewater <iamansel@gmail.com>
2017-11-01 23:35:48 -0700

    Refactored code to do read from dictionary document once per run.

--- dictionary_words.py
+++ dictionary_words.py
@@ -2,18 +2,29 @@ import sys
 from random import randint
 
 
-def random_sentence(sentence_length):
-    chosen_words = []
-
-    with open('/usr/share/dict/words', 'r') as f:
+def create_words_list(filename):
+    with open(filename, 'r') as f:
         dict_words = f.readlines()
-        for _ in range(input_sentence_len):
-            rand_num = randint(0, len(dict_words) - 1)
-            chosen_words.append(dict_words[rand_num].replace('\n', ''))
+    return dict_words
 
-    print(' '.join(chosen_words))
+
+def random_sentence(sentence_length, words_list):
+    '''
+    inputs:
+    what is does:
+    returns:
+    '''
+    chosen_words = []
+    dict_words = words_list
+
+    for _ in range(input_sentence_len):
+        rand_num = randint(0, len(dict_words) - 1)
+        chosen_words.append(dict_words[rand_num].replace('\n', ''))
+
+    return(' '.join(chosen_words))
 
 
 if __name__ == "__main__":
     input_sentence_len = int(sys.argv[1])
-    random_sentence(input_sentence_len)
+    new_word_list = create_words_list('/usr/share/dict/words')
+    print(random_sentence(input_sentence_len, new_word_list))
