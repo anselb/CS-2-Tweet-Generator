5af51b8c6e73085cec30ab71d03b0f21b3f111ca
Ansel Bridgewater <iamansel@gmail.com>
2017-12-21 00:21:33 -0800

    added regex line to remove most text in brackets

--- scraper/scrape_script.py
+++ scraper/scrape_script.py
@@ -2,6 +2,7 @@ from diffbot import DiffbotClient,DiffbotCrawl
 from config import API_TOKEN
 import pprint
 import time
+import re
 
 def get_article_text(article_url):
     print("Calling article API endpoint on the url: http://shitelonsays.com/transcript...\n")
@@ -18,12 +19,22 @@ def get_article_text(article_url):
 urls_file = open('pages.txt')
 output_file = open('corpus.txt', 'w')
 
-corpus = ''
-
+total_urls = 0
 for line in urls_file:
+    total_urls += 1
+
+urls_file = open('pages.txt')
+
+corpus = ''
+current_url = 1
+for line in urls_file:
+    print('Getting text from url ' + str(current_url) + ' out of ' + str(total_urls) + ' urls')
     url = line.strip()
     article = get_article_text(url)
     corpus += article
+    current_url += 1
 
-output_file.write(corpus)
+clean_corpus = re.sub(r'\[.*\]\s|\[.*\],|\[.*\]\.', '', corpus)
+
+output_file.write(clean_corpus)
 print('Done creating corpus')
