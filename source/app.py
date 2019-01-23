3da812ce1f711aea511661cbb6e0cb46dc8bb7d5
Ansel Bridgewater <iamansel@gmail.com>
2019-01-23 15:45:49 -0800

    Added config settings and template to reload quote with a button

--- source/app.py
+++ source/app.py
@@ -3,7 +3,13 @@ import histogram
 from markov import nth_markov_dictograms, nth_markov_chain
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+# Code to make viewing changes easier (avoids caching)
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 with open('../scraper/corpus.txt', 'r') as myfile:
     elon_corpus = myfile.read().replace('\n', '')
@@ -11,7 +17,8 @@ with open('../scraper/corpus.txt', 'r') as myfile:
 @app.route('/')
 def return_sentence():
     elon_markov_dict = nth_markov_dictograms(elon_corpus, 2)
-    return nth_markov_chain(elon_markov_dict)
+    sentence = nth_markov_chain(elon_markov_dict)
+    return render_template('index.html', message=sentence)
 
     # histo = histogram.histogram_dictionary('fish.txt')
     # return sample.weighted_dict_sampling(histo)
