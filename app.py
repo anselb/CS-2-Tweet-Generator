61ca765419b4815b7e869fa93f74bf39399ffe03
Ansel Bridgewater <iamansel@gmail.com>
2019-01-23 18:28:00 -0800

    Moved app.py and added paths for Flask to access right files

---
+++ app.py
@@ -0,0 +1,29 @@
+# To access files in source
+import sys
+sys.path.append('source/')
+
+import sample
+import histogram
+from markov import nth_markov_dictograms, nth_markov_chain
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__, template_folder='source/templates', static_folder ='source/static')
+
+# Code to make viewing changes easier (avoids caching)
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+with open('scraper/corpus.txt', 'r') as myfile:
+    elon_corpus = myfile.read().replace('\n', '')
+
+@app.route('/')
+def return_sentence():
+    elon_markov_dict = nth_markov_dictograms(elon_corpus, 2)
+    sentence = nth_markov_chain(elon_markov_dict)
+    print('test')
+    return render_template('index.html', message=sentence)
+
+    # histo = histogram.histogram_dictionary('fish.txt')
+    # return sample.weighted_dict_sampling(histo)
